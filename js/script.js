dc159b4eb3bf99b3ce8ac07af04e46cf8d7f3a07
Bruna Damazia <brunadamazia.dev@gmail.com>
2022-10-07 19:04:42 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,49 +1,68 @@
 //Ativar links do menu
 const links = document.querySelectorAll('.header-menu a')
 
-function ativarLink(link) {
-    const url = location.href
-    const href = link.href
-    if(url.includes(href)) {
-        link.classList.add('ativo')
-    }
+function ativarLink (link) {
+  const url = location.href
+  const href = link.href
+  if (url.includes(href)) {
+    link.classList.add('ativo')
+  }
 }
 
 links.forEach(ativarLink)
 
-
 //Ativar itens do orçamento
 const parametros = new URLSearchParams(location.search)
 
-function ativarProduto(parametro) {
-    const elemento = document.getElementById(parametro)
-    if(elemento) {
-        elemento.checked = true
-    }
-    
-console.log(elemento)
+function ativarProduto (parametro) {
+  const elemento = document.getElementById(parametro)
+  if (elemento) {
+    elemento.checked = true
+  }
+
+  console.log(elemento)
 }
 
 parametros.forEach(ativarProduto)
 
-
 // perguntas frequentes]
 
-const perguntas = document.querySelectorAll(".perguntas button")
+const perguntas = document.querySelectorAll('.perguntas button')
 
-function ativarPergunta(event) {
-    const pergunta = event.currentTarget
-    const controls = pergunta.getAttribute("aria-controls")
-    const resposta = document.getElementById(controls)
+function ativarPergunta (event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
 
-    resposta.classList.toggle("ativa")
-    const ativa = resposta.classList.contains('ativa')
-    console.log(ativa)
-    pergunta.setAttribute('aria-expended', 'true')
+  resposta.classList.toggle('ativa')
+  const ativa = resposta.classList.contains('ativa')
+  console.log(ativa)
+  pergunta.setAttribute('aria-expended', 'true')
 }
 
-function eventosPerguntas(pergunta) {
-    pergunta.addEventListener('click', ativarPergunta)
+function eventosPerguntas (pergunta) {
+  pergunta.addEventListener('click', ativarPergunta)
 }
 
 perguntas.forEach(eventosPerguntas)
+
+// Galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem (event) {
+  const img = event.currentTarget
+  galeriaContainer.prepend(img)
+}
+
+function eventosGaleria (img) {
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+// Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime()
+}
